61fd1e86a8e072cceb40f6d0afe447d6f1274903
aprianfirlanda <aprianfirlanda@gmail.com>
2024-09-05 22:20:03 +0700

    feat: update category_service_test.go run mock on create

--- service/category_service_test.go
+++ service/category_service_test.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"go-fiber-learn/app"
 	"go-fiber-learn/model/domain"
 	"go-fiber-learn/model/web"
@@ -14,10 +15,13 @@ func TestCategoryServiceImpl_Create(t *testing.T) {
 	app.LoadEnvironment("test")
 	db := app.OpenConnection()
 	categoryRepositoryMock := new(repository.CategoryRepositoryMock)
-	categoryRepositoryMock.Mock.On("Create", db, &domain.Category{Name: "spend"}).Return(nil)
+	categoryRepositoryMock.Mock.On("Create", db, &domain.Category{Name: "spend"}).Run(func(args mock.Arguments) {
+		category := args.Get(1).(*domain.Category)
+		category.ID = 1
+	}).Return(nil)
 	service := NewCategoryService(db, categoryRepositoryMock)
 	categoryId, err := service.Create(&web.CategoryCreateRequest{Name: "spend"})
 	assert.Nil(t, err, "Create Category return an error")
-	logrus.Info("categoryId :" + string(categoryId))
-	assert.NotEqual(t, 0, categoryId)
+	logrus.Infof("categoryId : %d", categoryId)
+	assert.NotEqual(t, int32(0), categoryId)
 }
